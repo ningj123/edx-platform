c2c5fb8f686af976332c71de13347d38e8d342c9
Matt Tuchfarber <mtuchfarber@edx.org>
2017-11-27 17:10:31 -0500

    Quality fixes

--- lms/static/js/program_marketing.js
+++ lms/static/js/program_marketing.js
@@ -4,10 +4,12 @@ function playVideo(src) {
     document.querySelector('#program_video iframe').style = 'display:block;';
     document.querySelector('#program_video iframe').src = src;
 }
-function expandFAQ(link, faq_hash){
-    var faq_preview_el = document.getElementById("preview-answer-" + faq_hash)
-    var faq_complete_el = document.getElementById("complete-answer-" + faq_hash)
-    link.classList.add("hidden")
-    faq_preview_el.classList.add("hidden")
-    faq_complete_el.classList.remove("hidden")
+function expandFAQ(currentElement, faqHash) {
+    'use strict';
+    var link = currentElement;
+    var faqPreviewElement = document.getElementById('preview-answer-' + faqHash);
+    var faqCompleteElement = document.getElementById('complete-answer-' + faqHash);
+    link.classList.add('hidden');
+    faqPreviewElement.classList.add('hidden');
+    faqCompleteElement.classList.remove('hidden');
 }
