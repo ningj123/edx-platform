e2674a375a4788f21a64dc061df3e3bf13a9c5b9
Matt Tuchfarber <mtuchfarber@edx.org>
2017-11-27 15:59:55 -0500

    Add program marketing FAQ expansion
    Content over 100 chars gets truncated and replaced with a  "See more" link. Full answer is shown when clicked.

--- lms/static/js/program_marketing.js
+++ lms/static/js/program_marketing.js
@@ -4,3 +4,10 @@ function playVideo(src) {
     document.querySelector('#program_video iframe').style = 'display:block;';
     document.querySelector('#program_video iframe').src = src;
 }
+function expandFAQ(link, faq_hash){
+    var faq_preview_el = document.getElementById("preview-answer-" + faq_hash)
+    var faq_complete_el = document.getElementById("complete-answer-" + faq_hash)
+    link.classList.add("hidden")
+    faq_preview_el.classList.add("hidden")
+    faq_complete_el.classList.remove("hidden")
+}
